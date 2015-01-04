4cc53f1ef9b4c1ac5e2c33862f2fdf43aebe76e8
Eugen Soloviov <suenot@gmail.com>
2015-01-04 14:47:27 +0400

    some

--- js/controllers.js
+++ js/controllers.js
@@ -4,9 +4,6 @@
 var app_c=angular.module('appControllers', []);
 
 app.controller('MainCtrl', function($scope){
-	$scope.useitem=usearray;
-	$scope.setModal=function(number){
-		$scope.modalSelected=usearray[number-1];
-	};
+
 });
 
